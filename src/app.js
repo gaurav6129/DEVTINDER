7298bcb7cdc24fd6f10c123ff006f5f9c023cb55
gaurav6129 <grai6129@gmail.com>
2025-03-06 12:41:27 +0530

    create delete api

--- src/app.js
+++ src/app.js
@@ -40,18 +40,43 @@ app.get("/users", async (req, res) => {
   //  req.send(users );
 });
 // Feed Api -Get / feed  get all the users from the database
-app.get("/feed",(req,res)=>{
-  try{
-    const users=User.find({});
+app.get("/feed", (req, res) => {
+  try {
+    const users = User.find({});
     res.send(users);
-  }
-  catch(err){
-    res.status(400).send("Error in fetching data:"+err.message);
+  } catch (err) {
+    res.status(400).send("Error in fetching data:" + err.message);
   }
   // const users=User.find({})
   // res.send(users);
+  app.get("/feed", async (req, res) => {
+    try {
+      const user = await User.findOne({});
+      if (!user) {
+        res.status(400).send("User not found");
+      } else {
+        res.send(user);
+      }
 
+      res.send(users);
+    } catch (err) {
+      res.status(400).send("error in fetching data:" + err.message);
+    }
+  });
 });
+app.delete("/user",async(req,res)=>{
+  const userId=req.body.userId;
+  try{
+    const user= await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  }
+  catch{
+    res.status(400).send("Error in deleting user:"+err.message);
+
+  }
+})
+
+
 connectDB()
   .then(() => {
     console.log("Connected to the database");
