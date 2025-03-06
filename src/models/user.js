9298a9c57de88f3df371452d8afc1e6b033a2253
gaurav6129 <grai6129@gmail.com>
2025-03-06 18:18:30 +0530

    update schema field:input

--- src/models/user.js
+++ src/models/user.js
@@ -2,18 +2,22 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
   firstName: {
     type: String,
+    required:true,
   },
   lastName: {
     type: String,
   },
   emailId: {
     type: String,
+    required:true,
   },
   password:{
     type:String,
+    required:true,
   },
   rollNumber:{
     type:String,
+    required:true,
   }
 });
 module.exports = mongoose.model("User", userSchema);
